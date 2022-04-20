001922939a77886c8ca7b4cb6d429960ac8483da
Enu Azubike <enuazubike88@gmail.com>
2022-04-20 17:22:39 +0100

    Updates

---
+++ precache-manifest.1fde19f6d8d1d8e3148fabf248889371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "022fac6eda9e6202f564",
+    "url": "/monsters-rolodex/static/css/main.cd7dbc60.chunk.css"
+  },
+  {
+    "revision": "022fac6eda9e6202f564",
+    "url": "/monsters-rolodex/static/js/main.022fac6e.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/monsters-rolodex/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "10992e196e3dd19b20c1",
+    "url": "/monsters-rolodex/static/js/2.10992e19.chunk.js"
+  },
+  {
+    "revision": "f444969ec7718d649e38",
+    "url": "/monsters-rolodex/static/js/runtime~main.f444969e.js"
+  },
+  {
+    "revision": "0b8ae1d5740de45c0222b48c7f03b4c3",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
